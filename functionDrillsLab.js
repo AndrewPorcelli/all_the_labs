f9cf22e423362496cc1c0187051c47d98abad42b
Andrew Porcelli <andy.porcelli1@gmail.com>
2022-05-31 17:13:40 -0600

    Finished question 21 as well now

--- functionDrillsLab.js
+++ functionDrillsLab.js
@@ -413,26 +413,43 @@ console.log(totalFrogs2);
 
 ////////////////// PROBLEM 21 ////////////////////
 let sampleArray = [0, 1, 2, 3, 4, 7, 5, 6, 8, 9];
+let normArray = [0, 1, 3, 3, 5];
 /*
   Write a function that takes in an array of numbers as an argument. In the body of the function, write logic to determine if the array is in ascending order. The function should return true, if it is sorted in ascending order, false if it is not. Create a variable, `arrayIsAscending` and set it equal to your function invoked. Use the sample array to test this function.
 */
 
 //CODE HERE
 
-// only returns true; need to come back and do this one
-const sortArray = function (arr) {
-  let sortArr = arr.sort((a, b) => a - b);
-  if (sortArr) {
-    return true;
-  } else {
-    return false;
+const checkArrayAscending = function (arr) {
+  let comparisonValue = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < comparisonValue) {
+      return false;
+    } else {
+      comparisonValue = arr[i];
+    }
   }
+  return true;
 };
-console.log(sortArray([1, 3, 2, 4]));
-const arrayIsAscending = sortArray(sampleArray);
-console.log(arrayIsAscending);
 
-////////////////// PROBLEM 22 ////////////////////
+let arrayIsAscending = checkArrayAscending(sampleArray);
+console.log(arrayIsAscending);
+console.log(checkArrayAscending(normArray));
+
+// only returns true; need to come back and do this one
+// const sortArray = function (arr) {
+//   let sortArr = arr.sort((a, b) => a - b);
+//   if (sortArr) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+// console.log(sortArray([1, 3, 2, 4]));
+// const arrayIsAscending = sortArray(sampleArray);
+// console.log(arrayIsAscending);
+
+// ////////////////// PROBLEM 22 ////////////////////
 
 let duck = "cute";
 
